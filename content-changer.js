c1114cc96deea60e1ced6b78fc088462438f4c71
clefalto <hamomelett@gmail.com>
2025-10-07 21:06:49 -0700

    add console typing thing

--- content-changer.js
+++ content-changer.js
@@ -1,5 +1,8 @@
 // find topbar element
 
+let currentContent = "home"; // content will be Home by default
+let typing = '';
+
 window.addEventListener('DOMContentLoaded', () => {
     var script = document.createElement('script');
     script.src = "//code.jquery.com/jquery.min.js";
@@ -14,20 +17,34 @@ window.addEventListener('DOMContentLoaded', () => {
     aboutButton.addEventListener('click', () => {changeContentTo("about.html")});
     gamesButton.addEventListener('click', () => {changeContentTo("games.html")});
     projectsButton.addEventListener('click', () => {changeContentTo("projects.html")});
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key.length == 1) {
+            typing = typing + event.key;
+        }
+        else if (event.key == 'Backspace' && typing.length > 0) {
+            typing = typing.substring(0, typing.length - 1);
+        }
+
+        let p = document.getElementById('path');
+        p.innerHTML = `visitor@clefalto ~/${currentContent} $ ${typing}<span class="blinking-text">▮</span>`;
+    });
+
+    changeContentTo("home.html");
 });
 
 
-
 function changeContentTo(html) {
-    console.log(html);
     $.get(html, function(data) {
         $(".main").html(data);
     });
 
     let p = document.getElementById("path");
     let pathname = html.replace(".html", "").replace("home", "");
-    p.innerHTML = `visitor@clefalto ~/${pathname} $<span class="blinking-text">▮</span>`;
+    p.innerHTML = `visitor@clefalto ~/${pathname} $ <span class="blinking-text">▮</span>`;
+    currentContent = pathname;
+    typing = "";
+
     document.title = `${html.replace(".html", "")} -- clefalto`;
 }
 
-changeContentTo("home.html");
